1426f4f2d9190f1eaad4ad41cf21c4ddb519dfa9
roldanmoncada <roldan.h.moncada@gmail.com>
2023-03-08 17:26:56 -0700

    implementing components and styles.

---
+++ my-react-app/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div className='nav-wrapper'>
+      <ul className='nav-content'>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Projects</li>
+      </ul>
+    </div>
+  )
+}
